e6c1982dea627ae1d4e2af79f6f08f08bbe5ecad
ZYevhenii <zinich911@gmail.com>
2023-11-01 19:35:05 +0200

    add connection settings

--- server.js
+++ server.js
@@ -12,18 +12,3 @@ app.use("/api", personRouter);
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
-
-pool.connect().then((client) => {
-  client
-    .query("select $1::text as name", ["pg-pool"])
-    .then((res) => {
-      client.release();
-      console.log("hello from", res.rows[0].name);
-    })
-    .catch((e) => {
-      client.release();
-
-      console.error("query error", e.message, e.stack);
-      process.exit(1);
-    });
-});
