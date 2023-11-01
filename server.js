3fec0953ea045b057db0c8674319ab0fbfdf8a60
ZYevhenii <zinich911@gmail.com>
2023-11-01 16:37:45 +0200

    add connection settings

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const personRouter = require("./routes/person.routes");
 require("dotenv").config();
+const pool = require("./db");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -10,4 +11,17 @@ app.use("/api", personRouter);
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
+});
+
+pool.connect().then((client) => {
+  client
+    .query("select $1::text as name", ["pg-pool"])
+    .then((res) => {
+      client.release();
+      console.log("hello from", res.rows[0].name);
+    })
+    .catch((e) => {
+      client.release();
+      console.error("query error", e.message, e.stack);
+    });
 });
